2d38692328ca6f051b169435c1a7a8dff9705042
David Best <david_best@ntlworld.com>
2019-06-02 08:50:37 +0100

    updated so exception works correctly, and updated tests

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
 class DockingStation:
 
     def __init__(self):
-        self.docked = False
+        pass
 
     def release_bike(self):
-        if self.docked == False:
-            raise Exception("No bike available")
-        else:
-            return self.docked
+        try:
+             return self.docked
+        except AttributeError as e:
+            raise e
+        finally:
+            if hasattr(self, 'docked'):
+                delattr(self, 'docked')
+
+        # if self.docked == False:
+        #     raise Exception("No bike available")
+        # else:
+        #     return self.docked
+        #     self.docked = False
 
     def dock_bike(self, bike):
         self.docked = bike
 
     def see_bike(self):
-        return self.docked
+        try:
+            return self.docked
+        except AttributeError as e:
+            raise e
 
 class Bike:
 
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,23 +1,27 @@
 import unittest
+import sys
+sys.path.append('..')
 from app import DockingStation, Bike
 
 class TestDockingStation(unittest.TestCase):
 
     def setUp(self):
         self.docking_station = DockingStation()
+        self.bike = Bike()
 
     def test_docking_station_object(self):
         self.assertIsInstance(self.docking_station, DockingStation)
 
     def test_for_docked_bike(self):
-        bike = Bike()
-        self.docking_station.dock_bike(bike)
-        self.assertEquals(self.docking_station.docked, bike)
-
+        self.docking_station.dock_bike(self.bike)
+        self.assertEqual(self.docking_station.docked, self.bike)
+        self.docking_station.release_bike()
+        with self.assertRaises(AttributeError):
+            self.docking_station.release_bike()
 
     def test_raises_exception_if_no_bike(self):
-        with self.assertRaises(Exception):
-            self.docking_station.release_bike()
+        with self.assertRaises(AttributeError):
+            self.docking_station.see_bike()
 
 
 class TestBike(unittest.TestCase):
@@ -30,7 +34,7 @@ class TestBike(unittest.TestCase):
         self.assertIsInstance(self.bike, Bike)
 
     def test_bike_is_working(self):
-        self.assertEquals(self.bike.is_working(), True)
+        self.assertEqual(self.bike.is_working(), True)
 
 
 if __name__ == '__main__':
